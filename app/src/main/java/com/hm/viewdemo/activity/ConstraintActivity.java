dae4488acf94dcf03a8b21628868a8afeb33f0f3
dumingwei <15764225790@139.com>
2017-03-14 10:30:31 +0800

    了解ConstraintLayout

---
+++ app/src/main/java/com/hm/viewdemo/activity/ConstraintActivity.java
@@ -0,0 +1,26 @@
+package com.hm.viewdemo.activity;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.hm.viewdemo.R;
+import com.hm.viewdemo.base.BaseActivity;
+
+public class ConstraintActivity extends BaseActivity {
+
+
+    public static void launch(Context context) {
+        Intent starter = new Intent(context, ConstraintActivity.class);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected int bindLayout() {
+        return R.layout.activity_constraint;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
