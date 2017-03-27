7e62c9342ae1f94b4a2ffccf167c655ca0453c71
dumingwei <15764225790@139.com>
2017-03-27 10:54:24 +0800

    了解百分比布局

---
+++ app/src/main/java/com/hm/viewdemo/activity/PercentLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.hm.viewdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hm.viewdemo.R;
+
+public class PercentLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_layout);
+    }
+}
