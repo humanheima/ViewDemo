61aa060cb67207947ae7a87e1589620414f17022
humanheima <humanheima@gmail.com>
2017-04-17 17:47:41 +0800

    研究了一下侧滑删除和自定义View时候的构造函数

---
+++ app/src/main/java/com/hm/viewdemo/activity/TestDragDeleteItemActivity.java
@@ -0,0 +1,145 @@
+package com.hm.viewdemo.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hm.viewdemo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TestDragDeleteItemActivity extends AppCompatActivity {
+
+    public static final String TAG = TestDragDeleteItemActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<String> stringList;
+    private RvAdapter adapter;
+    private LinearLayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_drag_delete_item);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        stringList = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            stringList.add("string" + i);
+        }
+        adapter = new RvAdapter(this, stringList);
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(TestDragDeleteItemActivity.this, "position=" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        adapter.setOnItemDeleteListener(new OnItemDeleteListener() {
+            @Override
+            public void onItemDelete(int position) {
+                stringList.remove(position);
+                adapter.notifyItemRemoved(position);
+            }
+        });
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    class RvAdapter extends RecyclerView.Adapter<RvAdapter.VH> {
+
+        private Context context;
+        private List<String> stringList;
+        private OnItemClickListener onItemClickListener;
+        private OnItemDeleteListener onItemDeleteListener;
+
+        public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        public void setOnItemDeleteListener(OnItemDeleteListener onItemDeleteListener) {
+            this.onItemDeleteListener = onItemDeleteListener;
+        }
+
+        public RvAdapter(Context context, List<String> stringList) {
+            this.context = context;
+            this.stringList = stringList;
+        }
+
+        @Override
+        public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_drag_delete, parent, false);
+            return new VH(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final VH holder, final int position) {
+            if (onItemClickListener != null) {
+                holder.itemLlContent.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.e(TAG, "onClick: position=" + position);
+                        onItemClickListener.onItemClick(v, holder.getAdapterPosition());
+                    }
+                });
+            }
+            if (onItemDeleteListener != null) {
+                holder.itemTextRemove.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onItemDeleteListener.onItemDelete(holder.getAdapterPosition());
+                    }
+                });
+            }
+            holder.itemImageBook.setImageResource(R.mipmap.ic_launcher);
+            holder.itemTextBookIsbn.setText(stringList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return stringList.size();
+        }
+
+        class VH extends RecyclerView.ViewHolder {
+
+            @BindView(R.id.item_image_book)
+            ImageView itemImageBook;
+            @BindView(R.id.item_text_book_name)
+            TextView itemTextBookName;
+            @BindView(R.id.item_text_book_isbn)
+            TextView itemTextBookIsbn;
+            @BindView(R.id.item_rl_book_top)
+            RelativeLayout itemRlBookTop;
+            @BindView(R.id.item_ll_content)
+            RelativeLayout itemLlContent;
+            @BindView(R.id.item_text_remove)
+            TextView itemTextRemove;
+
+            public VH(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    interface OnItemDeleteListener {
+        void onItemDelete(int position);
+    }
+}
