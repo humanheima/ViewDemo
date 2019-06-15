e0585eb74ad5681a68f1cadb6e6cc9c5f383bcdd
dumingwei <dumingwei07050@hellobike.com>
2019-06-15 16:29:28 +0800

    LayoutInflater review

--- app/src/main/java/com/hm/viewdemo/activity/LayoutInflateActivity.kt
+++ app/src/main/java/com/hm/viewdemo/activity/LayoutInflateActivity.kt
@@ -2,9 +2,12 @@ package com.hm.viewdemo.activity
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import com.hm.viewdemo.R
 import kotlinx.android.synthetic.main.activity_layout_inflate.*
 
@@ -12,6 +15,8 @@ import kotlinx.android.synthetic.main.activity_layout_inflate.*
  * LayoutInflater源码分析
  */
 class LayoutInflateActivity : AppCompatActivity() {
+
+    private val TAG = "LayoutInflateActivity"
 
     companion object {
 
@@ -25,10 +30,22 @@ class LayoutInflateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_inflate)
 
-        /*val inflater = LayoutInflater.from(this)
-        inflater.inflate(R.layout.activity_bottom_sheet,null)*/
-        val button = layoutInflater.inflate(R.layout.button_layout, null)
+        val inflater = LayoutInflater.from(this)
+        //val bottom=inflater.inflate(R.layout.activity_bottom_sheet,null)
+       val button = inflater.inflate(R.layout.button_layout, null)
+        //LayoutInflater.from()
         clRoot.addView(button)
-
     }
+
+    /*override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
+        val view = super.onCreateView(name, context, attrs)
+        Log.d(TAG, "onCreateView: $view")//是null
+        return view
+    }
+
+    override fun onCreateView(parent: View?, name: String?, context: Context?, attrs: AttributeSet?): View {
+        val view = super.onCreateView(parent, name, context, attrs)
+        Log.d(TAG, "onCreateView2: $view")//是null
+        return view
+    }*/
 }
