32c18507857bc5a0fca53d2abb804758012b64a9
forrest.du <forrest.du@ximalaya.com>
2021-07-16 15:14:44 +0800

    基本能用。

--- app/src/main/java/com/hm/viewdemo/activity/DrawEveryThingActivity.kt
+++ app/src/main/java/com/hm/viewdemo/activity/DrawEveryThingActivity.kt
@@ -4,16 +4,11 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.hm.viewdemo.R
-import com.hm.viewdemo.bean.SendOptionsBean
 import com.hm.viewdemo.util.ScreenUtil
 import com.hm.viewdemo.widget.CircleMaskView
-import kotlinx.android.synthetic.main.activity_draw_every_thing.*
-import java.util.*
 
 class DrawEveryThingActivity : AppCompatActivity() {
 
@@ -27,15 +22,6 @@ class DrawEveryThingActivity : AppCompatActivity() {
         }
     }
 
-    lateinit var stage3List: MutableList<SendOptionsBean>
-
-    lateinit var stage6List: MutableList<SendOptionsBean>
-
-    private var stage3FinishProgress = 0f
-
-    private var stage6FinishProgress = 0f
-
-    private lateinit var handler: Handler
 
     private lateinit var circleMaskView: CircleMaskView
 
@@ -44,50 +30,25 @@ class DrawEveryThingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw_every_thing)
-
-        handler = object : Handler() {
-            override fun handleMessage(msg: Message) {
-                super.handleMessage(msg)
-                Log.i(TAG, "handleMessage: ")
-                when (msg.what) {
-                    3 -> {
-                        updateStage3()
-
-                    }
-                    6 -> {
-                        updateStage6()
-
-                    }
-                }
-            }
-        }
-
-        stage3List = initStage3List()
-        stage6List = initStage6List()
-
-        setData()
-
-        btnUpdateStage3.setOnClickListener {
-            stage3FinishProgress = 0f
-            updateStage3()
-        }
-
-        btnUpdateStage6.setOnClickListener {
-            stage6FinishProgress = 0f
-
-            updateStage6()
-
-        }
-
         circleMaskView = findViewById(R.id.circle_mask_view)
 
-        val dpToPx = ScreenUtil.dpToPx(this, 200)
-        circleMaskView.setInnerRadius(dpToPx)
+        //初始半径，宽高的勾股定理，计算出直径是 382，半径就是191
+        val startPx = ScreenUtil.dpToPx(this, 191)
+        val finalPx = ScreenUtil.dpToPx(this, 14)
+
+        //控件一半的距离
+        val halfWidth = ScreenUtil.dpToPx(this, 249) / 2
+        val halfHeight = ScreenUtil.dpToPx(this, 289) / 2
+
+        circleMaskView.setSrcCanvasRadius(startPx)
+        circleMaskView.setTranslateXDistance(startPx - halfWidth)
+        //circleMaskView.setTranslateXDistance(halfWidth)
+        circleMaskView.setTranslateYDistance(startPx - halfHeight)
+        //circleMaskView.setTranslateYDistance(halfHeight)
+        circleMaskView.setInnerRadius(startPx)
         circleMaskView.post {
 
-            val initialInnerRadius = circleMaskView.measuredWidth
-
-            valueAnimator = ValueAnimator.ofInt(dpToPx, 30)
+            valueAnimator = ValueAnimator.ofInt(startPx, finalPx)
             valueAnimator.duration = 3000
             valueAnimator.addUpdateListener {
                 val animatedValue = it.animatedValue as Int
@@ -95,78 +56,9 @@ class DrawEveryThingActivity : AppCompatActivity() {
                 //circleMaskView.setPaintWidth(animatedValue)
                 circleMaskView.setInnerRadiusAndInValidate(animatedValue)
             }
-            valueAnimator.startDelay = 2000
+            //valueAnimator.startDelay = 2000
             valueAnimator.start()
         }
-    }
-
-    private fun setData() {
-        stageAwardView0.setData(stage3List, 3, 1f)
-
-        stageAwardView1.setData(stage6List, 6, 1f)
-    }
-
-    /**
-     * 更新第4阶段
-     */
-    private fun updateStage3() {
-        stage3FinishProgress += 0.01f
-        Log.i(TAG, "updateStage3: fl = $stage3FinishProgress")
-        if (stage3FinishProgress - 1f > 0.001) {
-            return
-        }
-        stageAwardView0.setData(stage3List, 3, stage3FinishProgress)
-        handler.sendEmptyMessageDelayed(3, 50)
-    }
-
-    /**
-     * 更新第7阶段
-     */
-    private fun updateStage6() {
-        stage6FinishProgress += 0.01f
-        Log.i(TAG, "updateStage6: fl = $stage6FinishProgress")
-        if (stage6FinishProgress - 1f > 0.001) {
-            return
-        }
-        stageAwardView1.setData(stage6List, 6, stage6FinishProgress)
-        handler.sendEmptyMessageDelayed(6, 50)
-    }
-
-    private fun initStage3List(): MutableList<SendOptionsBean> {
-        val list: MutableList<SendOptionsBean> = ArrayList()
-        for (i in 0 until 9) {
-            val optionsBean = SendOptionsBean()
-            optionsBean.segValue = 100 * i
-            optionsBean.setSegKey(i + 0.5)
-            if (i == 0) {
-                optionsBean.setStatus(1)
-            } else if (i < 3) {
-                optionsBean.setStatus(2)
-            }
-            list.add(optionsBean)
-        }
-        return list
-    }
-
-    private fun initStage6List(): MutableList<SendOptionsBean> {
-        val list: MutableList<SendOptionsBean> = ArrayList()
-        for (i in 0 until 9) {
-            val optionsBean = SendOptionsBean()
-            optionsBean.segValue = 100 * i
-            optionsBean.setSegKey(i + 0.5)
-            if (i == 0) {
-                optionsBean.setStatus(1)
-            } else if (i < 6) {
-                optionsBean.setStatus(2)
-            }
-            list.add(optionsBean)
-        }
-        return list
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        handler.removeCallbacksAndMessages(null)
     }
 
 
--- app/src/main/java/com/hm/viewdemo/widget/CircleMaskView.java
+++ app/src/main/java/com/hm/viewdemo/widget/CircleMaskView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.hm.viewdemo.R;
+import com.hm.viewdemo.util.ScreenUtil;
 
 /**
  * Created by dumingwei on 2017/3/4.
@@ -24,16 +26,38 @@ public class CircleMaskView extends View {
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final int DEFAULT_SIZE = 200;
     private PorterDuffXfermode xfermode;// 图形混合模式
+    private PorterDuffXfermode clear;// 图形混合模式
 
     private static final String TAG = "CircleMaskView";
 
     private int innerRadius;
+
+    //原图片的开始的半径
+    private int srcCanvasRadius;
+    //src需要平移这段距离以后，开始画圆
+    private int translateXDistance;
+    private int translateYDistance;
+
     private float radius;
 
     private Bitmap dstBitmap;
 
-    private Bitmap srcBitmap;
 
+    private Bitmap srcBitmap;
+    private Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Canvas canvas;
+
+    public void setSrcCanvasRadius(int srcCanvasRadius) {
+        this.srcCanvasRadius = srcCanvasRadius;
+    }
+
+    public void setTranslateXDistance(int translateXDistance) {
+        this.translateXDistance = translateXDistance;
+    }
+
+    public void setTranslateYDistance(int translateYDistance) {
+        this.translateYDistance = translateYDistance;
+    }
 
     public CircleMaskView(Context context) {
         this(context, null);
@@ -56,10 +80,32 @@ public class CircleMaskView extends View {
         //这行代码一定要加，不然 xfermode 会出现不可预料的效果
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+        clear = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
 
+        Bitmap temp = BitmapFactory.decodeResource(getResources(), R.drawable.scenery);
+        //dstBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.scenery);
+        //dstBitmap = ImageUtil.getSampledBitmapFromResource(getResources(), R.drawable.scenery, ScreenUtil.dpToPx(getContext(), 249), ScreenUtil.dpToPx(getContext(), 289));
+        int width = temp.getWidth();
+        int height = temp.getHeight();
+
+        int newWidth = ScreenUtil.dpToPx(getContext(), 249);
+        int newHeight = ScreenUtil.dpToPx(getContext(), 289);
+
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+
+        Matrix matrix = new Matrix();
+
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        //dstBitmap = Bitmap.createBitmap(temp, 0, 0, newWidth, newHeight);
+        dstBitmap = Bitmap.createBitmap(temp, 0, 0, width, height, matrix, true);
+
+        temp.recycle();
         //目标图片
-        dstBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.scenery);
-        Log.i(TAG, "CircleMaskView: " + dstBitmap.getWidth() + " height = " + dstBitmap.getHeight());
+        Log.i(TAG, "CircleMaskView: " + temp.getWidth() + " height = " + temp.getHeight() + " size = " + temp.getByteCount() / 1024 / 1024 + "MB");
+        Log.i(TAG, "CircleMaskView: " + dstBitmap.getWidth() + " height = " + dstBitmap.getHeight() + " size = " + dstBitmap.getByteCount() / 1024 / 1024 + "MB");
+
     }
 
     @Override
@@ -81,12 +127,17 @@ public class CircleMaskView extends View {
 
         radius = getMeasuredHeight() / 2f;
         Log.i(TAG, "onMeasure: " + getMeasuredWidth() + " " + getMeasuredHeight());
+
+
+        //dstBitmap.setWidth(getMeasuredWidth());
+        // dstBitmap.setHeight(getMeasuredHeight());
         //源图片
         srcBitmap = makeSrc();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        //canvas.drawColor(Color.BLUE);
         canvas.drawBitmap(dstBitmap, 0, 0, mPaint);
         mPaint.setXfermode(xfermode);
         canvas.drawBitmap(makeSrc(), 0, 0, mPaint);
@@ -94,14 +145,32 @@ public class CircleMaskView extends View {
     }
 
     private Bitmap makeSrc() {
-        Bitmap bm = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bm);
-        Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        if (srcBitmap == null) {
+            //srcBitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+            //直径
+            int diameter = srcCanvasRadius * 2;
+            srcBitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
+        }
+
+        if (canvas == null) {
+            canvas = new Canvas(srcBitmap);
+            //canvas.translate(-translateXDistance, -translateYDistance);
+        }
+
+
+        Log.i(TAG, "makeSrc: srcCanvasRadius = " + srcCanvasRadius + " getMeasuredWidth()  = " + getMeasuredWidth() + " getMeasuredHeight() = " + getMeasuredHeight());
+
+        //Canvas c = new Canvas(srcBitmap);
+
+        p.setXfermode(clear);
+        canvas.drawPaint(p);
+        p.setXfermode(null);
         //设置的透明度是FF
-        //p.setColor(0xFFFFFFFF);
-        p.setColor(Color.BLUE);
-        c.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, (1.5f * innerRadius / 2f), p);
-        return bm;
+        p.setColor(0xFFFFFFFF);
+        //canvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, (1.5f * innerRadius / 2f), p);
+        //canvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, innerRadius, p);
+        canvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, innerRadius, p);
+        return srcBitmap;
     }
 
     public void setInnerRadius(int radius) {
