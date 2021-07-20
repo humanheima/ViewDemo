a65b907947088272f17f0bfaa823c9dc0cd1ebca
forrest.du <forrest.du@ximalaya.com>
2021-07-20 15:46:21 +0800

    CircleMaskView完善，删不必要的代码。

--- app/src/main/java/com/hm/viewdemo/widget/CircleMaskView.java
+++ app/src/main/java/com/hm/viewdemo/widget/CircleMaskView.java
@@ -18,14 +18,17 @@ import com.hm.viewdemo.util.ScreenUtil;
 /**
  * Created by dumingwei on 2017/3/4.
  * 圆形遮罩层，动态改变遮罩的大小
+ * <p>
+ * 1. Canvas是比控件的宽高要大的。
+ * 2. 创建源Bitmap，是为了让整个源图层有像素。
+ * 3. 别忘了在构造函数中设置 setLayerType(View.LAYER_TYPE_SOFTWARE, null);
  */
 public class CircleMaskView extends View {
 
-    private int mColor;
     private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final int DEFAULT_SIZE = 200;
-    private PorterDuffXfermode xfermode;// 图形混合模式
-    private PorterDuffXfermode clear;// 图形混合模式
+    private PorterDuffXfermode dstInXfermode;// 图形混合模式
+    private PorterDuffXfermode clearMode;// 图形混合模式
 
     private static final String TAG = "CircleMaskView";
 
@@ -38,7 +41,7 @@ public class CircleMaskView extends View {
     private Bitmap dstBitmap;
     //源图层
     private Bitmap srcBitmap;
-    private final Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint srcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     private Canvas srcCanvas;
 
@@ -57,14 +60,12 @@ public class CircleMaskView extends View {
 
     public CircleMaskView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mColor = getResources().getColor(R.color.colorAccent);
-        mPaint.setColor(mColor);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setStrokeWidth(0f);
         //这行代码一定要加，不然 xfermode 会出现不可预料的效果
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
-        clear = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
+        dstInXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+        clearMode = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
 
         Bitmap temp = BitmapFactory.decodeResource(getResources(), R.drawable.scenery);
         int width = temp.getWidth();
@@ -114,7 +115,7 @@ public class CircleMaskView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(dstBitmap, 0, 0, mPaint);
-        mPaint.setXfermode(xfermode);
+        mPaint.setXfermode(dstInXfermode);
         canvas.drawBitmap(makeSrc(), 0, 0, mPaint);
         mPaint.setXfermode(null);
     }
@@ -133,13 +134,13 @@ public class CircleMaskView extends View {
         Log.i(TAG, "makeSrc: srcCanvasRadius = " + srcCanvasRadius + " getMeasuredWidth()  = " + getMeasuredWidth() + " getMeasuredHeight() = " + getMeasuredHeight());
 
         //清除源图层上的像素，这个一定要清除
-        p.setXfermode(clear);
-        srcCanvas.drawPaint(p);
-        p.setXfermode(null);
+        srcPaint.setXfermode(clearMode);
+        srcCanvas.drawPaint(srcPaint);
+        srcPaint.setXfermode(null);
         //设置的透明度是FF
-        p.setColor(0xFFFFFFFF);
-        //重新在源图层上绘制圆形，圆形的半径是不断缩小的
-        srcCanvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, innerRadius, p);
+        srcPaint.setColor(0xFFFFFFFF);
+        //重新在源图层上绘制圆形，圆形的半径是不断缩小的，圆心是控件的中心点
+        srcCanvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, innerRadius, srcPaint);
         return srcBitmap;
     }
 
@@ -153,7 +154,6 @@ public class CircleMaskView extends View {
     }
 
     public void setColor(int mColor) {
-        this.mColor = mColor;
         mPaint.setColor(mColor);
         invalidate();
     }
