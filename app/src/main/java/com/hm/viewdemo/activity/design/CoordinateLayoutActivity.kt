02f06b7981a74d0605452330f4a9ead1f4ede39f
p_dmweidu <p_dmweidu@tencent.com>
2024-09-04 13:59:09 +0800

    实现嵌套滑动的功能。

--- app/src/main/java/com/hm/viewdemo/activity/design/CoordinateLayoutActivity.kt
+++ app/src/main/java/com/hm/viewdemo/activity/design/CoordinateLayoutActivity.kt
@@ -2,15 +2,18 @@ package com.hm.viewdemo.activity.design
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Outline
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.hm.viewdemo.databinding.ActivityCoordinateLayoutBinding
-import com.hm.viewdemo.databinding.ItemNewsListBinding
+import com.hm.viewdemo.util.ScreenUtil
+import com.hm.viewdemo.widget.MyNestedScrollView2
 
 class CoordinateLayoutActivity : AppCompatActivity() {
 
@@ -32,72 +35,54 @@ class CoordinateLayoutActivity : AppCompatActivity() {
         binding = ActivityCoordinateLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Glide.with(this)
-            //.load("https://xxvirtualcharactercdn.xxsypro.com/8B0C6E618460014119F1537BBBEFEA18.jpg")
-            .load("https://imgservices-1252317822.image.myqcloud.com/coco/s11152023/bf3b4a97.jzoi4c.jpg")
-            .into(binding.ivBg)
-
-
         val lp = binding.ivBgMask.layoutParams as ViewGroup.MarginLayoutParams
 
         val initialTopMargin = lp.topMargin
 
         Log.i(TAG, "onCreate:  initialTopMargin = $initialTopMargin")
 
-        binding.appBarLayout.post {
+        binding.scrollView2.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            Log.i(
+                TAG,
+                "onScrollChange:   scrollY = $scrollY oldScrollY = $oldScrollY"
+            )
 
-            val appBarLp =
-                binding.appBarLayout.layoutParams as androidx.coordinatorlayout.widget.CoordinatorLayout.LayoutParams
-
-            val behavior =
-                appBarLp.behavior as? com.google.android.material.appbar.AppBarLayout.Behavior?
-            Log.i(TAG, "onCreate: behavior = $behavior")
-        }
-
-        binding.appBarLayout.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
-            Log.i(TAG, "verticalOffset: $verticalOffset")
-            lp.topMargin = initialTopMargin + verticalOffset
-
+            lp.topMargin = initialTopMargin - scrollY
             binding.ivBgMask.layoutParams = lp
 
         }
-//        binding.imgTop.setOnClickListener {
-//            val curTranslationY = binding.appBar.translationY
-//            Log.i(TAG, "curTranslationY: $curTranslationY")
-//            val oa = ObjectAnimator.ofFloat(binding.appBar, "translationY", curTranslationY, 300f)
-//            oa.duration = 2000
-//            oa.start()
-//        }
 
-//        val layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
-//        binding.recyclerView.layoutManager = layoutManager
-//        binding.recyclerView.adapter = RvAdapter()
-    }
-
-    class RvAdapter : androidx.recyclerview.widget.RecyclerView.Adapter<RvAdapter.VH>() {
-
-
-        override fun onCreateViewHolder(parent: ViewGroup, position: Int): VH {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(com.hm.viewdemo.R.layout.item_news_list, parent, false)
-            return VH(view)
+        binding.llMaskContainer.outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                view.clipToOutline = true
+                outline.setRoundRect(
+                    0,
+                    0,
+                    view.width,
+                    view.height,
+                    ScreenUtil.dpToPxFloat(this@CoordinateLayoutActivity, 23)
+                )
+            }
         }
 
-        override fun getItemCount(): Int {
-            return 100
-        }
-
-        override fun onBindViewHolder(holder: VH, position: Int) {
-            //holder.itemView.text_news.text = "hello world$position"
-            holder.binding.textNews.text = "hello world$position"
-        }
-
-
-        class VH(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView) {
-
-            val binding = ItemNewsListBinding.bind(itemView)
-
-        }
+        binding.scrollView2.setInterceptCallback(object : MyNestedScrollView2.InterceptCallback {
+            override fun dontIntercept(): Boolean {
+                val top = binding.tvTextContent.scrollY
+                Log.i(
+                    TAG,
+                    "dontIntercept: binding.tvTextContent.scrollY = " + top
+                )
+                if (top > 0) {
+                    return true
+                }
+                return false;
+            }
+        })
+        Glide.with(this)
+            //.load("https://xxvirtualcharactercdn.xxsypro.com/8B0C6E618460014119F1537BBBEFEA18.jpg")
+            .load("https://imgservices-1252317822.image.myqcloud.com/coco/s11152023/bf3b4a97.jzoi4c.jpg")
+            .into(binding.ivBg)
+        binding.tvTextContent.movementMethod = ScrollingMovementMethod.getInstance()
     }
 
 }
---
+++ app/src/main/java/com/hm/viewdemo/widget/MyNestedScrollView2.java
@@ -0,0 +1,118 @@
+package com.hm.viewdemo.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+import androidx.core.widget.NestedScrollView;
+
+/**
+ * Created by dumingwei on 2017/3/5.
+ */
+public class MyNestedScrollView2 extends NestedScrollView {
+
+    private int downX;
+    private int downY;
+    private int mTouchSlop;
+
+    private int mLastY = 0;
+
+    /**
+     * 向上可以滚动的最大距离，当滚动距离 >= maxScrollY时，不再拦截并且手指从下向上滚动的时候，不再拦截事件。
+     */
+    private int maxScrollY = 540;
+
+    private View innerView;
+
+    private static final String TAG = "MyNestedScrollView2";
+
+
+    public MyNestedScrollView2(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyNestedScrollView2(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+    }
+
+    public MyNestedScrollView2(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        int moveY = (int) ev.getRawY();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) ev.getRawX();
+                downY = (int) ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onInterceptTouchEvent: getScrollY() = " + getScrollY());
+
+                int deltaY = moveY - mLastY;
+                Log.i(TAG, "onInterceptTouchEvent: deltaY = " + deltaY);
+
+                /**
+                 * 1. 当滑动距离 >= maxScrollY时 && 并且手指从下向上滚动的时候，不再拦截事件。
+                 */
+//                if (getScrollY() >= maxScrollY) {
+//                    if (deltaY < 0) {
+//                        return false;
+//                    } else if (deltaY > 0) {
+//                        return false;
+//                    }
+//
+//                }
+
+                /**
+                 * 手指从下向上滚动的时候，如果滑动距离 >= maxScrollY时，不再拦截事件，让内部的控件处理
+                 */
+                if (deltaY < 0) {
+                    if (getScrollY() >= maxScrollY) {
+                        return false;
+                    }
+                } else if (deltaY > 0) {
+                    /**
+                     * 手指从上向下滚动，这个时候，如果内部的TextView getTop() >= 0 时，不再拦截，先让内部的TextView 回到顶部
+                     */
+                    if (interceptCallback != null && interceptCallback.dontIntercept()) {
+                        return false;
+                    }
+
+                }
+
+                break;
+            default:
+                break;
+        }
+        mLastY = moveY;
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    public interface InterceptCallback {
+        boolean dontIntercept();
+    }
+
+    private InterceptCallback interceptCallback;
+
+    public void setInterceptCallback(InterceptCallback interceptCallback) {
+        this.interceptCallback = interceptCallback;
+    }
+
+    public void setInnerView(View innerView) {
+        this.innerView = innerView;
+    }
+}
