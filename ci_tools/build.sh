04e6204ac12f030b0cace29d6ed2621c4cf3a3c6
dumingwei <dumingwei@zhumengdao.com>
2025-08-07 19:25:31 +0800

    另一种方式生成二维码

--- ci_tools/build.sh
+++ ci_tools/build.sh
@@ -17,3 +17,8 @@ else
     echo "Error: Invalid BUILD_TYPE. Use 'debug' or 'release'."
     exit 1
 fi
+
+
+APK_PATH="${WORKSPACE}/app/build/outputs/apk/debug/app-debug.apk"
+python3 -m pip install qrcode[pil] --user
+python3 -c "import qrcode; qrcode.make('${APK_PATH}').save('${WORKSPACE}/ci_tools/another_qrcode.png')"
